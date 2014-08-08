b2d729b1acb3747a6bbdf256aec5ae830f19db51
_beans <_beans@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-08 20:24:39 +0000

    Version 0.9.0
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-beans/tags/0.9.0@962708 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,9 @@
+<?php
+
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) 
+    exit();
+
+include_once(plugin_dir_path(__FILE__).'/includes/wc-beans-settings.php');
+
+delete_option(WC_Beans_Settings::OPT_NAME);
+?>
