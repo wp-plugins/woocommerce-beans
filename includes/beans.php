a5e757ed93ca4831bd314d2b57da2e4853a61374
_beans <_beans@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-05 10:47:10 +0000

    Version 0.9.11 Fix database install bug
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-beans/tags/0.9.11@1020150 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/beans.php
@@ -0,0 +1,167 @@
+<?PHP
+/**
+* Copyright 2014 Beans
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you may
+* not use this file except in compliance with the License. You may obtain
+* a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations
+* under the License.
+*/
+
+
+if (!function_exists('curl_init')) 
+    trigger_error('Beans needs the CURL PHP extension.');
+
+if (!function_exists('json_decode')) 
+    trigger_error('Beans needs the JSON PHP extension.');
+
+
+class BeansException extends Exception
+{
+    public function __construct($error=array()) 
+    {
+        if(!isset($error['code']))
+            $error['code'] = -1;
+        if(!isset($error['message']))
+            $error['message'] = '';
+            
+        parent::__construct($error['message'], $error['code']);
+    }
+}
+
+
+class Beans
+{
+    static $signature = 'PHP API';      // Set this value to whatever the API is used for 
+    static $fail_silently = true;       // Set this to false in dev env
+    
+    static $endpoint = "http://api.beans.cards/v1/";
+    static $domain = "www.beans.cards";
+    static $access_token_path = 'oauth/access_token';
+    
+    const VERSION = '1.3.0';
+    
+    private static $secret = "";
+    private static $error_log_type = 0;         // PHP error_log: $message_type  argument
+    private static $error_log_destination = ''; // PHP error_log: $destination   argument
+    private static $error_log_extra = '';       // PHP error_log: $extra_headers argument
+    
+    // Consider using init instead of object construct 
+    public function __construct($config) 
+    {
+        self::init('', $config);
+    }
+    
+    public static function init($secret_key)
+    {
+        self::$secret = $secret_key;
+    }
+    
+    public static function get($path, $arg=null, $fail_silently=null)
+    {       
+        return self::make_request($path, $arg, $fail_silently, 'GET');
+    }    
+    
+    public static function post($path, $arg=null, $fail_silently=null)
+    {       
+        return self::make_request($path, $arg, $fail_silently, 'POST');
+    }
+        
+    public static function delete($path, $arg=null, $fail_silently=null)
+    {       
+        return self::make_request($path, $arg, $fail_silently, 'DELETE');
+    }
+    
+    // Use This function to avoid handling errors
+    public static function set_error_log($type, $destination='', $extra='')
+    {
+        self::$error_log_type = $type;
+        self::$error_log_destination = $destination;
+        self::$error_log_extra = $extra;
+    }
+
+    public static function get_token_from_cookie($cookies=null, $fail_silently=null){
+        if(!$cookies) $cookies = $_COOKIE;
+                
+        if(isset($cookies['beans_user'])){
+            $code = $cookies['beans_user'];
+            return Beans::get(self::$access_token_path, array('code'=>$code), $fail_silently);
+        }
+        return null;
+    }
+    
+    private static function make_request($path, $data=null, $fail_silently=null, $method=null)
+    {
+        $url = self::$endpoint.$path;
+        if(!$data) $data = array();
+        $ua = array(
+            'bindings_version'  => self::VERSION,
+            'application'       => self::$signature,
+            'lang'              => 'PHP',
+            'lang_version'      => phpversion(),
+            'publisher'         => 'Beans',
+        );  
+        $data_string = json_encode($data);
+        // Set Request Options
+        $curlConfig = array(
+            CURLOPT_URL            => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_CUSTOMREQUEST  => $method,
+            CURLOPT_POSTFIELDS     => $data_string, 
+            CURLOPT_HTTPAUTH       => CURLAUTH_BASIC,
+            CURLOPT_USERPWD        => self::$secret.':',
+            CURLOPT_HTTPHEADER     => array(                                                                          
+                'Content-Type: application/json',                                                                                
+                'Content-Length: ' . strlen($data_string),
+                'X-Beans-Client-User-Agent: '. json_encode($ua),
+            )
+        );
+        
+        //Make HTTP Request
+        $ch = curl_init();
+        curl_setopt_array($ch, $curlConfig);
+        $response = curl_exec($ch);
+        $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $content_type = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+        curl_close($ch);
+        
+        // Check for HTTP Error
+        if($content_type!='application/json'){
+            $error = array(
+                'code' => $http_status,
+                'message' => 'HTTP Error: '.$http_status,
+            );
+            return self::handle_error($error, $fail_silently);
+        }
+        
+        // Load response
+        $response = json_decode($response, TRUE);
+        
+        if($response['error'])
+            return self::handle_error($response['error'], $fail_silently);
+        
+        return $response['result'];
+    }
+    
+    private static function handle_error($error_string, $fail_silently=null)
+    {
+        if( $fail_silently === null )
+            $fail_silently = self::$fail_silently;
+        
+        $error = new BeansException($error_string);
+        
+        if($fail_silently){
+            error_log($error, self::$error_log_type, self::$error_log_destination, self::$error_log_extra);
+            return null;
+        }
+        else
+            throw $error;
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,96 @@
+=== WooCommerce · Beans · Loyalty & Reward Program  ===
+
+Contributors: _beans
+Tags: loyalty, reward, voucher, point, points, loyalty program, woocommerce, marketing, advertising, referring, discount, promoting, coupon, loyalbeans, beans, affiliate, e-commerce, ecommerce, engage, customers, woocommerce plugin, woocommerce extension, facebook, like, social network, credit, free
+Requires at least: 3.5
+Tested up to: 4.0.0
+Stable tag: 0.9.11
+License: Apache License, Version 2.0
+License URI: http://www.apache.org/licenses/LICENSE-2.0
+
+Beans is a powerful loyalty and reward program that helps you engage your customers.
+
+
+== Description ==
+
+Turn your customers into repeat customers by engaging them into your reward program. 
+Beans is a credit base system that allows you to reward your customers:
+- When they make a purchase.
+- When they like your facebook page.
+- and more to come...
+
+When your customers complete an action (for example like your facebook page), they will be credited
+an amount of beans credit that they can redeem in your shop or save for a later purchase.
+
+
+[Read more about Beans](http://bit.ly/1smNKtw)
+
+[Live Demo](http://bit.ly/1pHkqud)
+
+
+== Installation ==
+
+= Minimum Requirements =
+- WooCommerce >= 2.0
+- Supported currency are: USD, EUR, GBP, CAD
+
+= Automatic installation =
+1. Log in to your WordPress admin panel, navigate to the Plugins menu and click Add New. 
+2. Look for "Beans" and click Install Now and activate the plugin
+
+= Manual installation =
+1. Download the plugin file to your computer and unzip it 
+2. Upload the plugin folder to your WordPress installation’s wp-content/plugins/ directory and activate the plugin
+
+
+= Setting up =
+1. [Register](http://bit.ly/XPfxWP) for **free** to get your private and secret keys.
+2. Log in to your WordPress admin panel, navigate to WooCommerce Menu, go to Beans Submenu and enter your keys.
+
+
+== Frequently Asked Questions ==
+
+= How can you modify the style? =
+There is a stylesheet in the folder assets/css/
+that can be modified to match your website style.
+
+
+== Screenshots ==
+
+1. Beans display on the cart page.
+2. Beans display on the product page.
+3. Beans preview for the user.
+4. User account with transaction history.
+
+
+== Changelog ==
+
+= 0.9.11 =
+- Fix database install bug
+
+= 0.9.10 =
+- Fix beans display on product page
+
+= 0.9.9 = 
+- Multiples Bugs Fixes
+- Style Bugs Fixes
+- Performance improved
+- Add Info Log
+
+= 0.9.8 =
+- New Version (beta)
+- Integration has been improved
+- Beans account are now linked to the user account
+- You can reward your users for facebooks likes
+
+= 0.9.1 =
+- Bug fixes: When user want to change a reward
+- Enhancement: Improve Currency support
+
+= 0.9.0 =
+*Pre-release
+
+== Upgrade Notice == 
+
+Make sure you keep a copy of your stylesheet.
+ 
